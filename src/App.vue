1256507e814f252785480d473df2b8bcd2d95079
mj1000-70 <103566973+mj1000-70@users.noreply.github.com>
2022-04-14 14:44:48 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+<h1>{{title}}</h1>
+  <input class="" type="text" ref="name">
+  <button class="text-lg" @click="handleClick">Click here</button>
+  <p>Welcome</p>
+  <div v-if="showModel">
+    <second-view :header="header" :text="text"></second-view>
+  </div>
+<button @click="toggleModal">
+  <span v-if="showModel">Hide</span>
+  <span v-if="!showModel">hid</span>
+</button>
+</template>
+<script>
+
+import SecondView from "@/components/Second";
+export default {
+  name: 'App',
+  components: {SecondView},
+  data() {
+    return{
+      title:'The last Lord',
+      header:'The sing and ring',
+      text:'Show the lord',
+      showModel: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModel=!this.showModel
+    },
+    handleClick(){
+      console. log (this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+
+
+    }
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Second.vue
@@ -0,0 +1,17 @@
+<template>
+<h1>Second</h1>
+  <h2>{{header}}</h2>
+  <p>{{text}}</p>
+
+</template>
+
+<script>
+export default {
+props:['header','text'],
+  name: "SecondView"
+}
+</script>
+
+<style scoped>
+
+</style>
